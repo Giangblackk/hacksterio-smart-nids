fbca51750ef70d643b4a3edade80e119847aa087
giangblackk <giangblackk@gmail.com>
2020-12-07 17:25:09 +0700

    train model after model with clear_session

--- train_anomaly_detector.py
+++ train_anomaly_detector.py
@@ -28,7 +28,7 @@ def create_auto_encoder(input_size, beta, activation="relu"):
 
 if __name__ == "__main__":
     batch_size = 512
-    n_epoches = 1000
+    n_epoches = 10
     # load dataset
     dataset_file = "capEC2AMAZ-O4EL3NG-172.31.69.26a.pcap.h5"
     dataset = keras.utils.HDF5Matrix(dataset_file, "dataset")
@@ -43,56 +43,69 @@ if __name__ == "__main__":
     mapper_file = "mapper.json"
 
     with open(mapper_file, "r") as f:
-        mapper = json.load(f)
-    print(mapper)
+        feature_mapper = json.load(f)
+    print(feature_mapper)
 
-    # create models based on feature mapper
-    first_mapper = mapper[0]
-    first_mapper_dataset = numpy_dataset[:, np.array(first_mapper)]
-    first_mapper_dataset = np.reshape(
-        first_mapper_dataset, (-1, 1, 1, len(first_mapper))
-    )
+    ensemble_models = []
+    reconstructed_dataset = []
+    for index, mapper in enumerate(feature_mapper):
+        # extract dataset corresonding to mapper
+        mapper_dataset = numpy_dataset[:, np.array(mapper)]
+        mapper_dataset = np.reshape(mapper_dataset, (-1, 1, 1, len(mapper)))
+        print(mapper_dataset.shape)
 
-    print(first_mapper_dataset.shape)
+        # create models based on feature mapper
+        model_input = keras.Input(shape=(1, 1, len(mapper)), name=f"input_{index}")
+        model_seq = create_auto_encoder(len(mapper), 0.75)
 
-    first_input = keras.Input(shape=(1, 1, len(first_mapper)))
-    first_seq = create_auto_encoder(len(first_mapper), 0.75)
-    first_output = first_seq(first_input)
+        model_output = model_seq(model_input)
 
-    first_model = keras.Model(first_input, first_output)
+        model = keras.Model(model_input, model_output)
 
-    first_model.summary()
-    print(first_model.outputs)
+        model.summary()
+        print(model.outputs)
 
-    logdir = "logs/first_model/" + datetime.now().strftime("%Y%m%d-%H%M%S")
-    tensorboard_callback = keras.callbacks.TensorBoard(
-        log_dir=logdir, histogram_freq=1, write_images=True
-    )
+        # train model
+        logdir = "logs/ensemble_model_{}/".format(index) + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-    first_model.compile(
-        optimizer=tf.keras.optimizers.Adam(),
-        loss="binary_crossentropy",
-        # loss="mse",
-        metrics=[keras.metrics.RootMeanSquaredError(name="rmse")],
-    )
+        tensorboard_callback = keras.callbacks.TensorBoard(
+            log_dir=logdir, histogram_freq=1, write_images=True
+        )
 
-    first_model.fit(
-        first_mapper_dataset,
-        first_mapper_dataset,
-        shuffle=True,
-        epochs=n_epoches,
-        batch_size=batch_size,
-        validation_split=0.2,
-        callbacks=[tensorboard_callback,],
-    )
+        model.compile(
+            optimizer=tf.keras.optimizers.Adam(),
+            # loss="binary_crossentropy",
+            loss="mse",
+            metrics=[keras.metrics.RootMeanSquaredError(name="rmse")],
+        )
 
-    first_model.save(
-        "models/first_model/first.h5"
-    )  # cannot save Model Subclasses to .h5, use function instead
-    tf_session = keras.backend.get_session()
-    # write out tensorflow checkpoint & meta graph
-    saver = tf.train.Saver()
-    save_path = saver.save(tf_session, "models/first_model/first_model.ckpt")
+        model.fit(
+            mapper_dataset,
+            mapper_dataset,
+            shuffle=True,
+            epochs=n_epoches,
+            batch_size=batch_size,
+            validation_split=0.2,
+            callbacks=[tensorboard_callback,],
+        )
+        os.makedirs("models/ensemble_model_{0}/".format(index), exist_ok=True)
+        model.save("models/ensemble_model_{0}/model_{0}.h5".format(index))
+
+        reconstructed_dataset.append(model.predict(mapper_dataset))
+
+        # predict on input dataset as input for output model
+        keras.backend.clear_session()
+    
+    # concat reconstructed outputs
+
+    # create output model and train on reconstructed outputs
+    output_model_len = sum(map(lambda x: len(x), feature_mapper))
+    print("output_model_len:", output_model_len)
+
+    # tf_session = keras.backend.get_session()
+    # # write out tensorflow checkpoint & meta graph
+    # saver = tf.train.Saver()
+    # save_path = saver.save(tf_session, "models/first_model/first_model.ckpt")
 
     # train each ensemble models and save to files
     # train output model and save to file
