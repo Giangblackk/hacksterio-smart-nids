18d37852ef74748313e05e7549c2e24464e7f0e0
giangblackk <giangblackk@gmail.com>
2020-12-08 12:23:06 +0700

    how to freeze graph

--- scripts.sh
+++ scripts.sh
@@ -1,2 +1,10 @@
 # run experiments
 sudo python -m experiments.scapy_sniff_1
+
+# example for freeze cpkt
+CUDA_VISIBLE_DEVICES=-1 python freeze_graph.py \
+    --input_meta_graph models/total2/total.ckpt.meta \
+    --input_checkpoint models/total2/total.ckpt \
+    --output_graph models/total2/total.pb \
+    --output_node_names=ae_output_1/conv2d_1/Relu \
+    --input_binary=true
